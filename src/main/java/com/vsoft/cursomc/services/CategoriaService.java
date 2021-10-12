e23013f93c299a2c7da897cc8e2e82c7fbc66320
Victor Soler Pessoa Giraldello <victor.giraldello@gmail.com>
2021-10-12 17:04:42 -0300

    Criando repository e service para Categoria

---
+++ src/main/java/com/vsoft/cursomc/services/CategoriaService.java
@@ -0,0 +1,22 @@
+package com.vsoft.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.vsoft.cursomc.domain.Categoria;
+import com.vsoft.cursomc.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+
+	@Autowired
+	private CategoriaRepository repo;
+	
+	public Categoria find(Integer id) {
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElse(null);
+	} 
+	
+}
