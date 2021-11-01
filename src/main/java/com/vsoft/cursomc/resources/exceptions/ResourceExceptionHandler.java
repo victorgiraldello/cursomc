55f88934adf382f17d4462fcb6459f2dcb029b07
Victor Soler Pessoa Giraldello <victor.giraldello@gmail.com>
2021-10-31 21:33:01 -0300

    Deletando uma Categoria com DELETE

--- src/main/java/com/vsoft/cursomc/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/vsoft/cursomc/resources/exceptions/ResourceExceptionHandler.java
@@ -2,6 +2,7 @@ package com.vsoft.cursomc.resources.exceptions;
 
 import javax.servlet.http.HttpServletRequest;
 
+import com.vsoft.cursomc.services.exception.DataIntegrityException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -18,6 +19,14 @@ public class ResourceExceptionHandler {
 		StandardError err = new StandardError(HttpStatus.NOT_FOUND.value(), e.getMessage(), System.currentTimeMillis());
 		
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
+	}
+
+	@ExceptionHandler(DataIntegrityException.class)
+	public ResponseEntity<StandardError> dataIntegrityExceptionResponseEntity(DataIntegrityException e, HttpServletRequest request) {
+
+		StandardError err = new StandardError(HttpStatus.BAD_REQUEST.value(), e.getMessage(), System.currentTimeMillis());
+
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
 	}
 	
 }
--- src/main/java/com/vsoft/cursomc/services/CategoriaService.java
+++ src/main/java/com/vsoft/cursomc/services/CategoriaService.java
@@ -2,7 +2,9 @@ package com.vsoft.cursomc.services;
 
 import java.util.Optional;
 
+import com.vsoft.cursomc.services.exception.DataIntegrityException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.vsoft.cursomc.domain.Categoria;
@@ -29,6 +31,15 @@ public class CategoriaService {
 	public Categoria update(Categoria obj){
 		find(obj.getId());
 		return repo.save(obj);
+	}
+
+	public void delete(Integer id){
+		try{
+			repo.deleteById(id);
+		} catch(DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir Categorias que possuem produtos!");
+		}
+
 	}
 
 }
