86197c5b69059818e612304fdebca909011b61ee
Victor Soler Pessoa Giraldello <victor.giraldello@gmail.com>
2021-11-04 21:16:44 -0300

    PUT, DELETE e GET para Cliente

--- src/main/java/com/vsoft/cursomc/domain/Cliente.java
+++ src/main/java/com/vsoft/cursomc/domain/Cliente.java
@@ -43,10 +43,9 @@ public class Cliente implements Serializable {
 	@OneToMany(mappedBy = "cliente")
 	private List<Pedido> pedidos = new ArrayList<>();
 
-	public Cliente() {
-		super();
+	public Cliente(){
+
 	}
-	
 
 	public Cliente(Integer id, String nome, String email, String cpfOuCnpj, TipoCliente tipo) {
 		super();
@@ -54,7 +53,7 @@ public class Cliente implements Serializable {
 		this.nome = nome;
 		this.email = email;
 		this.cpfOuCnpj = cpfOuCnpj;
-		this.tipo = tipo.getCod();
+		this.tipo = (tipo == null)  ? null : tipo.getCod();
 	}
 
 
--- src/main/java/com/vsoft/cursomc/services/ClienteService.java
+++ src/main/java/com/vsoft/cursomc/services/ClienteService.java
@@ -1,11 +1,20 @@
 package com.vsoft.cursomc.services;
 
+import com.vsoft.cursomc.domain.Categoria;
 import com.vsoft.cursomc.domain.Cliente;
+import com.vsoft.cursomc.dto.CategoriaDTO;
+import com.vsoft.cursomc.dto.ClienteDTO;
 import com.vsoft.cursomc.repositories.ClienteRepository;
+import com.vsoft.cursomc.services.exception.DataIntegrityException;
 import com.vsoft.cursomc.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -17,5 +26,43 @@ public class ClienteService {
         Optional<Cliente> obj = repo.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException(
         "Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
+    }
+
+    public Cliente insert(Cliente obj) {
+        return repo.save(obj);
+    }
+
+    public Cliente update(Cliente obj){
+        Cliente newObj = find(obj.getId());
+        updateData(newObj, obj);
+        return repo.save(newObj);
+    }
+
+    private void updateData(Cliente newObj, Cliente obj) {
+        newObj.setNome(obj.getNome());
+        newObj.setEmail(obj.getEmail());
+    }
+
+    public void delete(Integer id){
+        try{
+            repo.deleteById(id);
+        } catch(DataIntegrityViolationException e) {
+            throw new DataIntegrityException("Não é possível excluir porque há entidades relacionadas!");
+        }
+
+    }
+
+    public List<Cliente> findAll(){
+        return repo.findAll();
+    }
+
+    public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+        PageRequest pageRequest = PageRequest.of(page, linesPerPage, Sort.Direction.valueOf(direction),
+                orderBy);
+        return repo.findAll(pageRequest);
+    }
+
+    public Cliente fromDTO(ClienteDTO objDto){
+        return new Cliente(objDto.getId(), objDto.getNome(), objDto.getEmail(), null, null);
     }
 }
