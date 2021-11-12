6ae68d497cc8d605096888efbc137c292d483b9c
Victor Soler Pessoa Giraldello <victor.giraldello@gmail.com>
2021-11-12 00:49:11 -0300

    Subtotal e total do Pedido

--- src/main/java/com/vsoft/cursomc/domain/ItemPedido.java
+++ src/main/java/com/vsoft/cursomc/domain/ItemPedido.java
@@ -29,6 +29,10 @@ public class ItemPedido {
         this.preco = preco;
     }
 
+    public double getSubTotal(){
+        return (preco-desconto) * quantidade;
+    }
+
     @JsonIgnore
     public Pedido getPedido(){
         return id.getPedido();
