79dd1448896f53d72e17fe24b1116af118a74ceb
Victor Soler Pessoa Giraldello <victor.giraldello@gmail.com>
2022-01-27 20:17:10 -0300

    Usando padrão de nomes para imagens

--- src/main/java/com/vsoft/cursomc/domain/Cliente.java
+++ src/main/java/com/vsoft/cursomc/domain/Cliente.java
@@ -47,8 +47,6 @@ public class Cliente implements Serializable {
 	@OneToMany(mappedBy = "cliente")
 	private List<Pedido> pedidos = new ArrayList<>();
 
-	private String imageUrl;
-
 	public Cliente(){
 		addPerfil(Perfil.CLIENTE);
 	}
@@ -142,14 +140,6 @@ public class Cliente implements Serializable {
 
 	public void setPedidos(List<Pedido> pedidos) {
 		this.pedidos = pedidos;
-	}
-
-	public String getImageUrl() {
-		return imageUrl;
-	}
-
-	public void setImageUrl(String imageUrl) {
-		this.imageUrl = imageUrl;
 	}
 
 	@Override
--- src/main/java/com/vsoft/cursomc/services/ClienteService.java
+++ src/main/java/com/vsoft/cursomc/services/ClienteService.java
@@ -14,6 +14,7 @@ import com.vsoft.cursomc.services.exception.AuthorizationException;
 import com.vsoft.cursomc.services.exception.DataIntegrityException;
 import com.vsoft.cursomc.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -24,6 +25,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.awt.image.BufferedImage;
 import java.net.URI;
 import java.util.List;
 import java.util.Optional;
@@ -41,6 +43,11 @@ public class ClienteService {
 
     @Autowired
     private S3Service s3Service;
+
+    @Autowired ImageService imageService;
+
+    @Value("${img.prefix.client.profile}")
+    private String prefix;
 
     public Cliente find(Integer id) {
         UserSS user = UserService.authenticated();
@@ -116,11 +123,10 @@ public class ClienteService {
             throw new AuthorizationException("Acesso negado.");
         }
 
-        URI uri = s3Service.uploadFile(multipartFile);
+        BufferedImage jpgImage = imageService.getJpgImageFromFile(multipartFile);
+        String fileName = prefix + user.getId() +".jpg";
 
-        Cliente cli = repo. findByEmail(user.getUsername());
-        cli.setImageUrl(uri.toString());
-        repo.save(cli);
+        URI uri = s3Service.uploadFile(imageService.getInputStream(jpgImage,"jpg"), fileName, "image");
 
         return uri;
     }
